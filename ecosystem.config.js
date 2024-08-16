ac74e048874471e214d89a44605b7f8e77d0d151
nick <nickynn07@gmail.com>
2024-08-16 14:04:36 -0400

    chore: Update ecosystem.config.js to use ts-node for running the server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: 'whapi',
-      script: 'node_modules/.bin/ts-node',
-      args: 'src/server.ts',
-      interpreter: 'node',
-      instances: 'max',
-      exec_mode: 'cluster',
-      max_restarts: 5,
-      restart_delay: 1000,
-      watch: false,
+      name: 'whapi', // Nombre de la aplicación
+      script: 'src/server.ts', // Ruta al archivo principal de tu aplicación
+      instances: 'max', // Usa el máximo número de instancias posibles
+      exec_mode: 'cluster', // Ejecuta en modo cluster para balanceo de carga
       env: {
-        NODE_ENV: 'development',
+        NODE_ENV: 'development', // Configura el entorno de desarrollo
         MONGODB_URI: 'mongodb://localhost:27017/whatsapp-bot-db',
         PORT: 4000,
       },
       env_production: {
-        NODE_ENV: 'production',
+        NODE_ENV: 'production', // Configura el entorno de producción
         MONGODB_URI: 'mongodb://localhost:27017/whatsapp-bot-db',
         PORT: 4000,
       },
-      error_file: './logs/whapi-err.log',
-      out_file: './logs/whapi-out.log',
-      log_file: './logs/whapi-combined.log',
-      time: true,
     },
   ],
 }
