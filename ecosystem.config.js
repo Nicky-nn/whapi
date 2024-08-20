9d1c22b37ab2e6fc02ee1da9cec164bcb297681b
nick <nickynn07@gmail.com>
2024-08-20 16:23:56 -0400

    chore: Actualizar opciones de reinicio y observación en el archivo de configuración

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'whapi',
       script: 'src/server.ts',
-      interpreter: 'ts-node', // Cambiado a `ts-node`
+      interpreter: 'ts-node',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
@@ -16,6 +16,14 @@ module.exports = {
         MONGODB_URI: 'mongodb://localhost:27017/whapi',
         PORT: 4000,
       },
+      // Nuevas opciones para reiniciar automáticamente
+      restart_delay: 5000, // Esperar 5 segundos antes de reiniciar
+      max_restarts: 5, // Máximo de 5 reintentos
+      watch: true, // Observar cambios y reiniciar cuando se detecten
+      ignore_watch: ['node_modules', '.git'], // Excluir estos directorios de la observación
+      watch_options: {
+        followSymlinks: false, // No seguir enlaces simbólicos
+      },
     },
   ],
-}
+};
